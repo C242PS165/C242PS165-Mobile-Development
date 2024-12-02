b3478a648d10a5ff2cd59405c8ee91c9e5615a14
MD-Tyas <a393b4ky3108@bangkit.academy>
2024-12-02 22:35:32 +0700

    Menghapus prediksi cuaca hari ini

--- app/src/main/java/com/tyas/smartfarm/view/pages/fragment/WeatherFragment.kt
+++ app/src/main/java/com/tyas/smartfarm/view/pages/fragment/WeatherFragment.kt
@@ -10,8 +10,6 @@ import com.tyas.smartfarm.R
 import com.tyas.smartfarm.databinding.FragmentWeatherBinding
 import com.tyas.smartfarm.view.adapter.DailyForecastAdapter
 import com.tyas.smartfarm.view.adapter.DailyWeather
-import com.tyas.smartfarm.view.adapter.HourlyForecastAdapter
-import com.tyas.smartfarm.view.adapter.HourlyWeather
 
 class WeatherFragment : Fragment() {
     private var _binding: FragmentWeatherBinding? = null
@@ -27,23 +25,6 @@ class WeatherFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        val hourlyData = listOf(
-            HourlyWeather("12:00", "27°", R.drawable.ic_weather),
-            HourlyWeather("13:00", "26°", R.drawable.ic_weather),
-            HourlyWeather("14:00", "26°", R.drawable.ic_weather),
-            HourlyWeather("15:00", "26°", R.drawable.ic_weather),
-            HourlyWeather("16:00", "26°", R.drawable.ic_weather),
-            HourlyWeather("17:00", "26°", R.drawable.ic_weather),
-            HourlyWeather("18:00", "26°", R.drawable.ic_weather),
-            HourlyWeather("19:00", "26°", R.drawable.ic_weather),
-            HourlyWeather("20:00", "26°", R.drawable.ic_weather),
-            HourlyWeather("21:00", "26°", R.drawable.ic_weather)
-        )
-
-        val hourlyAdapter = HourlyForecastAdapter(hourlyData)
-        binding.hourlyForecastRecycler.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
-        binding.hourlyForecastRecycler.adapter = hourlyAdapter
 
         val dailyData = listOf(
             DailyWeather("Hari ini", "Kabut asap", "33° / 25°", R.drawable.ic_weather),
