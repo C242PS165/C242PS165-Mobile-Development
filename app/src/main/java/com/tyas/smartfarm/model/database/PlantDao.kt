2104453a5b5c410cae5a358e964d44a221ee8912
MD-Tyas <a393b4ky3108@bangkit.academy>
2024-12-03 22:46:16 +0700

    Memperbaiki kelas PlantDao.kt sebagai lapisan abstraksi untuk mengakses dan memodifikasi data tanaman dalam database.

--- app/src/main/java/com/tyas/smartfarm/model/database/PlantDao.kt
+++ app/src/main/java/com/tyas/smartfarm/model/database/PlantDao.kt
@@ -2,7 +2,6 @@ package com.tyas.smartfarm.model.database
 
 import androidx.lifecycle.LiveData
 import androidx.room.*
-import com.tyas.smartfarm.model.database.Plant  // Pastikan ini yang diimpor!
 
 @Dao
 interface PlantDao {
@@ -11,10 +10,4 @@ interface PlantDao {
 
     @Query("SELECT * FROM plants")
     fun getAllPlants(): LiveData<List<Plant>>
-
-    @Delete
-    suspend fun deletePlant(plant: Plant)
-
-    @Update
-    suspend fun updatePlant(plant: Plant)
 }
