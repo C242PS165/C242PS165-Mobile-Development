a346647db1b680bde4776dec118759efc18bb45e
MD-Tyas <a393b4ky3108@bangkit.academy>
2024-12-09 10:03:48 +0700

    Menambah userId pada Plant.kt untuk memperbaiki tambah catatan tanaman agar bisa sesuai berdasarkan authentikasi user.

--- app/src/main/java/com/tyas/smartfarm/model/Plant.kt
+++ app/src/main/java/com/tyas/smartfarm/model/Plant.kt
@@ -2,6 +2,7 @@ package com.tyas.smartfarm.model
 
 data class Plant(
     val id: String = "",
+    val userId: String = "",
     val name: String = "",
     val category: String = "",
     val description: String = "",
